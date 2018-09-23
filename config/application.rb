d887586688623571c4fabba0fbc262949843489b
Manuel Ortega <manuel@lasindias.coop>
2018-09-22 22:10:17 -0500

    Updated production environment file

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,5 @@ module Estaciont
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = false
-    Rails.application.routes.default_url_options[:host] = 'https://estaciont-backend.herokuapp.com'
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,5 +83,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: "https://estaciont-backend.herokuapp.com" }
+  config.active_storage.service = :amazon
+  config.action_mailer.default_url_options = { host: 'https://estaciont-backend.herokuapp.com' }
+  config.default_url_options = { host: "https://estaciont-backend.herokuapp.com" }  
 end
